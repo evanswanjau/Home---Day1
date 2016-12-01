dcce853bc0cce26bbd68a187f189f670496884fa
Evans Wanjau <evanswanjau@gmail.com>
2016-12-01 16:30:03 +0300

    Add files via upload
    2nd commit including car.py

---
+++ Home-Day1/Andelabs/car.py
@@ -0,0 +1,23 @@
+class Car(self):
+  car_type = type(self) is Car
+  name = 'General'.self
+  model = 'GM'.self
+  
+  def car_doors():
+    number_of_doors = 4
+    if name = Car('Koenigsegg', 'Agera R') or name = Car('Porshe', '911 Turbo'):
+      number_of_doors = 2
+  
+  def car_wheels():
+    number_of_wheels = 4
+    global man
+    man = Car('MAN', 'Truck', 'Trailer')
+    if name = Car(man):
+      number_of_wheels = 8
+      
+  def car_speed():
+    parked_speed = 0
+    moving_speed = 77
+    
+  def drive_car():
+    car = car_type.moving_speed(77)
